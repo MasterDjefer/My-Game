234fad01457131a0940dfbc81017c603b8fa6707
MasterDjefer <kostia1998@uk.net>
2016-10-01 15:02:26 +0300

    Add function for choosing level.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <conio.h>
 #include <cstdlib>
-#define n 8
-#define m 8
+#define n 10
+#define m 10
 using namespace std;
 
 class Table
@@ -51,7 +51,46 @@ protected:
         for (int i = 4; i < 7; ++i)
             for (int j = 5; j < 7; ++j)
                 mass[i][j] = '#';
+        for (int i = 1; i < 9; ++i)
+            for (int j = 7; j < 9; ++j)
+            {
+                mass[i][j] = '#';
+                mass[j][i] = '#';
+            }
+    }
+    void set_side_level2(char mass[n][m])
+    {
+        for (int i = 0; i < 4; ++i)
+            for (int j = 0; j < 4; ++j)
+                mass[i][j] = '#';
+        for (int i = 0; i < 4; ++i)
+            for (int j = 6; j < m-1; ++j)
+            {
+                mass[i][j] = '#';
+                mass[j][i] = '#';
+            }
+        for (int i = 6; i < n-1; ++i)
+            for (int j = 6; j < m-1; ++j)
+                mass[i][j] = '#';
+    }
+    void set_side_level3(char mass[n][m])
+    {
+        for (int i = 1; i < 9; ++i)
+        {
+            mass[6][i] = '#';
+            mass[7][i] = '#';
+            mass[8][i] = '#';
+        }
+        for (int i = 1; i < 7; ++i)
+            mass[i][8] = '#';
 
+        mass[1][1] = '#';
+        mass[1][4] = '#';
+        mass[1][7] = '#';
+        mass[2][7] = '#';
+        mass[3][4] = '#';
+        mass[5][4] = '#';
+        mass[5][1] = '#';
     }
     void show(char mass[n][m])
     {
@@ -72,8 +111,8 @@ protected:
     int target_x;
     int target_y;
     int number;
-    int hand[100];
-    int target[100];
+    int hand[10];
+    int target[10];
 protected:
     void set_elements_level1()
     {
@@ -89,6 +128,33 @@ protected:
         target[4] = 4; target[5] = 1;
         target[6] = 6; target[7] = 4;
     }
+    void set_elements_level2()
+    {
+        number = 8;
+        head_x = 4;
+        head_y = 5;
+        hand[0] = 4; hand[1] = 4;
+        hand[2] = 5; hand[3] = 3;
+        hand[4] = 5; hand[5] = 5;
+        hand[6] = 6; hand[7] = 4;
+        target[0] = 1; target[1] = 5;
+        target[2] = 4; target[3] = 8;
+        target[4] = 5; target[5] = 1;
+        target[6] = 8; target[7] = 4;
+    }
+    void set_elements_level3()
+    {
+        number = 6;
+        head_x = 3;
+        head_y = 2;
+        hand[0] = 2; hand[1] = 4;
+        hand[2] = 2; hand[3] = 5;
+        hand[4] = 4; hand[5] = 4;
+        target[0] = 4; target[1] = 7;
+        target[2] = 5; target[3] = 7;
+        target[4] = 6; target[5] = 7;
+    }
+
     void set_head(char mass[n][m])
     {        
         mass[head_x][head_y] = 'o';
@@ -114,12 +180,37 @@ public:
     {
         set_null(mass);
         set_side(mass);
-        set_side_level1(mass);
-        set_elements_level1();
+        choose_level();
+    }    
+    void choose_level()
+    {
+        cout << "Enter your level(1-3): ";
+        int choice = 0;
+        cin >> choice;
+        system("cls");
+        switch (choice)
+        {
+            case 1:
+                set_side_level1(mass);
+                set_elements_level1();
+                break;
+            case 2:
+                set_side_level2(mass);
+                set_elements_level2();
+                break;
+            case 3:
+                set_side_level3(mass);
+                set_elements_level3();
+                break;
+            default:
+                cout << "Bad inputing or this level doesn\'t exist!\n";
+                choose_level();
+        }
         set_head(mass);
         set_hand(mass);
         set_target(mass);
-    }    
+    }
+
     int search(int a, int b)
     {
         for (int i = 0; i < n; i+=2)
